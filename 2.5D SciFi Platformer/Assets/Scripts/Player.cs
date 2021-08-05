2fbe9ff61b28f61cff1a40b8cf558fb060fed2c6
Lane Watson <44728749+lanewatson@users.noreply.github.com>
2021-08-05 01:52:27 -0400

    Player horizontal movement script

---
+++ 2.5D SciFi Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    [SerializeField] private float _speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get horizontal input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //define direction based on input
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        
+        _controller.Move(direction * Time.deltaTime);
+    }
+}
